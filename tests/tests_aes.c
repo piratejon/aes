88c907b3990904c639e7366301604b92fd0eb7a4
Jonathan Wesley Stone <js@piratejon.com>
2014-04-29 21:34:38 -0500

    NIST SP 800-38A ECB tests pass

--- tests/tests_aes.c
+++ tests/tests_aes.c
@@ -623,15 +623,14 @@ static void test_CBC_Mode ( void )
 }
 
 static void test_InconteamVectors ( void ) {
-  Byte * key_ecb_128_enc = "2b7e151628aed2a6abf7158809cf4f3c";
 
+  Byte * key_ecb_128_enc = "2b7e151628aed2a6abf7158809cf4f3c";
   Byte * tv_ecb_128_enc[] = {
     "6bc1bee22e409f96e93d7e117393172a",
     "ae2d8a571e03ac9c9eb76fac45af8e51",
     "30c81c46a35ce411e5fbc1191a0a52ef",
     "f69f2445df4f9b17ad2b417be66c3710",
   };
-
   Byte * ct_ecb_128_enc[] = {
     "3ad77bb40d7a3660a89ecaf32466ef97",
     "f5d3d58503b9699de785895a96fdbaaf",
@@ -640,15 +639,69 @@ static void test_InconteamVectors ( void ) {
   };
 
   Byte * key_ecb_128_dec = "2b7e151628aed2a6abf7158809cf4f3c";
-
   Byte * tv_ecb_128_dec[] = {
     "3ad77bb40d7a3660a89ecaf32466ef97",
     "f5d3d58503b9699de785895a96fdbaaf",
     "43b1cd7f598ece23881b00e3ed030688",
     "7b0c785e27e8ad3f8223207104725dd4",
   };
-
   Byte * pt_ecb_128_dec[] = {
+    "6bc1bee22e409f96e93d7e117393172a",
+    "ae2d8a571e03ac9c9eb76fac45af8e51",
+    "30c81c46a35ce411e5fbc1191a0a52ef",
+    "f69f2445df4f9b17ad2b417be66c3710",
+  };
+
+  Byte * key_ecb_192_enc = "8e73b0f7da0e6452c810f32b809079e562f8ead2522c6b7b";
+  Byte * tv_ecb_192_enc[] = {
+    "6bc1bee22e409f96e93d7e117393172a",
+    "ae2d8a571e03ac9c9eb76fac45af8e51",
+    "30c81c46a35ce411e5fbc1191a0a52ef",
+    "f69f2445df4f9b17ad2b417be66c3710",
+  };
+  Byte * ct_ecb_192_enc[] = {
+    "bd334f1d6e45f25ff712a214571fa5cc",
+    "974104846d0ad3ad7734ecb3ecee4eef",
+    "ef7afd2270e2e60adce0ba2face6444e",
+    "9a4b41ba738d6c72fb16691603c18e0e",
+  };
+
+  Byte * key_ecb_192_dec = "8e73b0f7da0e6452c810f32b809079e562f8ead2522c6b7b";
+  Byte * tv_ecb_192_dec[] = {
+    "bd334f1d6e45f25ff712a214571fa5cc",
+    "974104846d0ad3ad7734ecb3ecee4eef",
+    "ef7afd2270e2e60adce0ba2face6444e",
+    "9a4b41ba738d6c72fb16691603c18e0e",
+  };
+  Byte * pt_ecb_192_dec[] = {
+    "6bc1bee22e409f96e93d7e117393172a",
+    "ae2d8a571e03ac9c9eb76fac45af8e51",
+    "30c81c46a35ce411e5fbc1191a0a52ef",
+    "f69f2445df4f9b17ad2b417be66c3710",
+  };
+
+  Byte * key_ecb_256_enc = "603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4";
+  Byte * tv_ecb_256_enc[] = {
+    "6bc1bee22e409f96e93d7e117393172a",
+    "ae2d8a571e03ac9c9eb76fac45af8e51",
+    "30c81c46a35ce411e5fbc1191a0a52ef",
+    "f69f2445df4f9b17ad2b417be66c3710",
+  };
+  Byte * ct_ecb_256_enc[] = {
+    "f3eed1bdb5d2a03c064b5a7e3db181f8",
+    "591ccb10d410ed26dc5ba74a31362870",
+    "b6ed21b99ca6f4f9f153e7b1beafed1d",
+    "23304b7a39f9f3ff067d8d8f9e24ecc7",
+  };
+
+  Byte * key_ecb_256_dec = "603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4";
+  Byte * tv_ecb_256_dec[] = {
+    "f3eed1bdb5d2a03c064b5a7e3db181f8",
+    "591ccb10d410ed26dc5ba74a31362870",
+    "b6ed21b99ca6f4f9f153e7b1beafed1d",
+    "23304b7a39f9f3ff067d8d8f9e24ecc7",
+  };
+  Byte * pt_ecb_256_dec[] = {
     "6bc1bee22e409f96e93d7e117393172a",
     "ae2d8a571e03ac9c9eb76fac45af8e51",
     "30c81c46a35ce411e5fbc1191a0a52ef",
@@ -682,6 +735,74 @@ static void test_InconteamVectors ( void ) {
   for ( i = 0; i < 4; i += 1 ) {
     tv = HexString_To_Array(tv_ecb_128_dec[i]);
     ct = HexString_To_Array(pt_ecb_128_dec[i]);
+
+    InvCipher ( tv->raw, key->raw );
+
+    for ( j = 0; j < tv->length; j += 1 ) {
+      ASSERT ( tv->raw[j] == ct->raw[j], "Wrong plaintext." );
+    }
+
+    free_bytestr ( ct );
+    free_bytestr ( tv );
+  }
+  free_bytestr(key);
+
+  SetMode(FIPS_AES_192);
+  key = HexString_To_Array(key_ecb_192_enc);
+  for ( i = 0; i < 4; i += 1 ) {
+    tv = HexString_To_Array(tv_ecb_192_enc[i]);
+    ct = HexString_To_Array(ct_ecb_192_enc[i]);
+
+    Cipher ( tv->raw, key->raw );
+
+    for ( j = 0; j < tv->length; j += 1 ) {
+      ASSERT ( tv->raw[j] == ct->raw[j], "Wrong ciphertext." );
+    }
+
+    free_bytestr ( ct );
+    free_bytestr ( tv );
+  }
+  free_bytestr(key);
+
+  SetMode(FIPS_AES_192);
+  key = HexString_To_Array(key_ecb_192_dec);
+  for ( i = 0; i < 4; i += 1 ) {
+    tv = HexString_To_Array(tv_ecb_192_dec[i]);
+    ct = HexString_To_Array(pt_ecb_192_dec[i]);
+
+    InvCipher ( tv->raw, key->raw );
+
+    for ( j = 0; j < tv->length; j += 1 ) {
+      ASSERT ( tv->raw[j] == ct->raw[j], "Wrong plaintext." );
+    }
+
+    free_bytestr ( ct );
+    free_bytestr ( tv );
+  }
+  free_bytestr(key);
+
+  SetMode(FIPS_AES_256);
+  key = HexString_To_Array(key_ecb_256_enc);
+  for ( i = 0; i < 4; i += 1 ) {
+    tv = HexString_To_Array(tv_ecb_256_enc[i]);
+    ct = HexString_To_Array(ct_ecb_256_enc[i]);
+
+    Cipher ( tv->raw, key->raw );
+
+    for ( j = 0; j < tv->length; j += 1 ) {
+      ASSERT ( tv->raw[j] == ct->raw[j], "Wrong ciphertext." );
+    }
+
+    free_bytestr ( ct );
+    free_bytestr ( tv );
+  }
+  free_bytestr(key);
+
+  SetMode(FIPS_AES_256);
+  key = HexString_To_Array(key_ecb_256_dec);
+  for ( i = 0; i < 4; i += 1 ) {
+    tv = HexString_To_Array(tv_ecb_256_dec[i]);
+    ct = HexString_To_Array(pt_ecb_256_dec[i]);
 
     InvCipher ( tv->raw, key->raw );
 
